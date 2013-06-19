7e5b5a18a2b7376498a103a7c93cfc1b11556631
Bianca <gando.bia@gmail.com>
2013-06-18 17:06:21 -0700

    annotation plugin functioning using localstorage

--- Serverside/controllers/works.js
+++ Serverside/controllers/works.js
@@ -11,9 +11,6 @@ module.exports.list = function(req, res){
       works.push({title : play.TITLE, _id: play._id})
       // idMap[play.TITLE] = play._id;
     })
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.json(works);
    });
 };
@@ -22,9 +19,6 @@ module.exports.findById = function(req, res) {
   var query = Play.findById(req.params.id, function(err, resp) {
     // console.log("err", err);
     // console.log("resp", resp);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.json(resp);
   });
 
   // db.collection('plays', function(err, collection) {
